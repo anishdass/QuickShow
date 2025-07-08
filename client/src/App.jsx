8ea5eb44ac5cd384e6e5535489c687ba8b77f6c2
anishdass <anishdassatoffice@gmail.com>
2025-07-08 15:51:04 +0100

    Navbar completed

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import MyBookings from "./pages/MyBookings";
+import Movies from "./pages/Movies";
+import SeatLayout from "./pages/SeatLayout";
+import Favorite from "./pages/Favorite";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  const isAdmin = useLocation().pathname.startsWith("/admin");
+
+  return (
+    <>
+      <Toaster />
+      {!isAdmin && <Navbar />}
+      <Routes>
+        <Route path='/' element={Home} />
+        <Route path='/movies' element={Movies} />
+        <Route path='/movies/:id' element={MovieDetails} />
+        <Route path='/movies/:id/:date' element={SeatLayout} />
+        <Route path='/my-bookings' element={MyBookings} />
+        <Route path='/favorite' element={Favorite} />
+      </Routes>
+      {!isAdmin && <Footer />}
+    </>
+  );
+};
+
+export default App;
