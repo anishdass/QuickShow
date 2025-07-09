54de44514cf87e76b8763bd2049a53eff5f07847
anishdass <anishdassatoffice@gmail.com>
2025-07-09 13:18:44 +0100

    Hero section completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from "../components/sections/Home/HeroSection";
 import FeaturedSection from "../components/sections/Home/FeaturedSection";
 import TrailerSection from "../components/sections/Home/TrailerSection";
+import Footer from "../components/sections/Footer";
 
 const Home = () => {
   return (
