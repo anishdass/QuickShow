ab4a94ca39e3371be4fee5fd39af27e63aed398b
anishdass <anishdassatoffice@gmail.com>
2025-07-16 15:50:14 +0100

    Db implementation done

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(cors());
 
 // Routes
 app.get("/", (req, res) => res.send("Server is Live!"));
+await connectDB();
 
 app.listen(port, () =>
   console.log(`Server Listening at http://localhost:${port}`)
