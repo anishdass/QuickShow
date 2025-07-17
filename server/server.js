827e134818492db4a908d777b8bd00fce267b1f3
anishdass <anishdassatoffice@gmail.com>
2025-07-17 11:40:02 +0100

    implementing inngest

--- server/server.js
+++ server/server.js
@@ -2,18 +2,24 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// App start and DB connect
+await connectDB();
+app.listen(port, () =>
+  console.log(`Server Listening at http://localhost:${port}`)
+);
+
 // Middlewares
 app.use(express.json());
+app.use(clerkMiddleware());
 app.use(cors());
 
 // Routes
 app.get("/", (req, res) => res.send("Server is Live!"));
-await connectDB();
-
-app.listen(port, () =>
-  console.log(`Server Listening at http://localhost:${port}`)
-);
+app.use("/api/inngest", serve({ client: inngest, functions }));
